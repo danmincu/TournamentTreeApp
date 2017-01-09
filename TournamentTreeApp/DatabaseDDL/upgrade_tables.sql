2ffd8d79f1d1185ca414317b1d7e8c4eb4c382a6
Dan Mincu <danmincu@gmail.com>
2017-01-09 14:12:56 -0500

    Upgrade script from first version to 1.1

---
+++ TournamentTreeApp/DatabaseDDL/upgrade_tables.sql
@@ -0,0 +1,14 @@
+﻿
+ALTER TABLE Division
+ADD [RoundRobin] [bit] NOT NULL DEFAULT(0),
+	[DoubleElimination] [bit] NOT NULL DEFAULT(0),
+	[NoSecondaryFinal] [bit] NOT NULL DEFAULT(0),
+	[NoComebackFromLooserBracket] [bit] NOT NULL DEFAULT(0),
+	[ConsolidationRound] [bit] NOT NULL DEFAULT(0)
+
+ALTER TABLE Tournament
+ADD [RoundRobin] [bit] NOT NULL DEFAULT(0),
+	[DoubleElimination] [bit] NOT NULL DEFAULT(0),
+	[NoSecondaryFinal] [bit] NOT NULL DEFAULT(0),
+	[NoComebackFromLooserBracket] [bit] NOT NULL DEFAULT(0),
+	[ConsolidationRound] [bit] NOT NULL DEFAULT(0)
