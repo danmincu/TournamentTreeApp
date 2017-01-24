31d2d497bff9477d9fa5d6f4ccda26d1206de376
Dan Mincu <danmincu@gmail.com>
2017-01-23 23:49:00 -0500

    Added fast lane for adding a participant to a division

---
+++ TournamentTreeApp/Models/FastParticipant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TournamentsTreeApp.Models
+{
+    public class FastParticipant
+    {
+        public string ParticipantName { set; get; }
+        public Guid SchoolId { set; get; }
+        public string SchoolName { set; get; }
+        public Guid DivisionId { set; get; }
+        public Guid TournamentId { set; get; }
+    }
+}
